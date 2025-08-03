91b675abac6ad60e8d1961ff3e2b121bcff67e66
navi-04 <naveenrajthiyagarajan6@gmail.com>
2025-08-03 22:14:13 +0530

    added sample test

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+todos = []
 
 @app.route('/')
 def home():
-    return 'Hello, Heroku! Your Python app is running globally.'
+    return "✅ Hello, this Flask app is running on Render for FREE!"
+
+@app.route('/todos', methods=['GET', 'POST'])
+def todo_list():
+    if request.method == 'POST':
+        task = request.json.get('task')
+        if task:
+            todos.append(task)
+            return jsonify({"message": "Task added", "todos": todos})
+        else:
+            return jsonify({"error": "Task is required"}), 400
+    return jsonify(todos)
 
 if __name__ == '__main__':
     app.run(debug=True)

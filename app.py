f04e2d3dd14af979a51da3f138584655e0ea0416
navi-04 <naveenrajthiyagarajan6@gmail.com>
2025-08-03 22:20:43 +0530

    added sample test

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-todos = []
-
 @app.route('/')
 def home():
     return "✅ Hello, this Flask app is running on Render for FREE!"
 
 @app.route('/todos', methods=['GET', 'POST'])
 def todo_list():
+    global todos
+
     if request.method == 'POST':
         task = request.json.get('task')
-        if task:
-            todos.append(task)
-            return jsonify({"message": "Task added", "todos": todos})
-        else:
-            return jsonify({"error": "Task is required"}), 400
+    elif request.method == 'GET' and 'task' in request.args:
+        task = request.args.get('task')
+    else:
+        task = None
+
+    if task:
+        todos.append(task)
+        return jsonify({"message": "Task added", "todos": todos})
+
     return jsonify(todos)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
